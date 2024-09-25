a1774132c8199ee9b6bbbeb4b6bc26e76f9ce301
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-09-25 13:09:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 connectDatabase();
 app.use(express.json());
 app.use(cors({
-    origin:"https://clever-capybara-b585a7.netlify.app/",
+    origin:"*",
     credential:true,
     
 }));
